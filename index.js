2f0f1b3c9b9fdfde9838366d62bdd21fd96e5ea8
sasankmoulidharreddy <164542157+sasankmoulidharreddy@users.noreply.github.com>
2024-03-22 23:17:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3700;
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+var midPort = app.listen(port, function () {
+    console.log('Server is Started.......... ' + port);
+})
+
+var io = require('socket.io').listen(midPort);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'Welcome to the Real Time Web Chat' });
+    socket.emit('message', { message: 'Developed by sasank' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
